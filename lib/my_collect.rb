e4dfbb3dfff41a3113f87ecbd141e47e09632f47
Nathan Miller <natem31190@gmail.com>
2020-12-18 12:39:36 -0500

    commit tree 93bc8fee7001ddafec85bce9738e3d783afbced1

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(array) 
+    counter = 0 # sets up empty array
+    collection = [] # sets up empty array
+    while counter < array.length
+        collection << yield(array[counter]) # shovel array in collection
+        counter += 1 # increments for each iteration
+    end
+    collection #returns modified colleciton
+end
